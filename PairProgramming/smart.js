dc8eb8dea495fe1286a6df72d8c2c5828781eade
jhuang3047 <60354062+jhuang3047@users.noreply.github.com>
2020-04-22 11:10:26 -0400

    updated

--- PairProgramming/smart.js
+++ PairProgramming/smart.js
@@ -1,17 +1,15 @@
-console.log("How Smart Are You?");
+htmlelement.onclick() = function(){
+    
 var theScore = 0 ;
-const wrongButton = document.querySelector("#wrongButton","squarewrongButton1","squarewrongButton2","squarewrongButton3","circlewrongButton1","circlewrongButton2","circlewrongButton3","rectanglewrongButton1","rectanglewrongButton2","rectanglewrongButton3");
-const correctButton =document.querySelector("#correctButton","#squarecorrectButton","#rectanglecorrectButton","circlecorrectButton")
-const theText = document.querySelector('h2')
-wrongButton.addEventListener('click',isClicked);
-function isClicked(){
-    console.log("clicked"); 
-    theScore = +0;
+var wrongButton = document.getElementById("wrongButton","squarewrongButton1","squarewrongButton2","squarewrongButton3","circlewrongButton1","circlewrongButton2","circlewrongButton3","rectanglewrongButton1","rectanglewrongButton2","rectanglewrongButton3");
+var correctButton =document.getElementById("correctButton","squarecorrectButton","rectanglecorrectButton","circlecorrectButton")
+var theText = document.querySelector('h2')
+
+correctButton.onclick = function(){
+    theScore = +1
 }
-correctButton.addEventListener('click',isClicked2);
-function isClicked2(){
-    console.log("clicked"); 
-    theScore = +1;
+wrongButton.onclick = function(){
+    theScore =+0
 }
 
 if (theScore <= 4) {
@@ -23,4 +21,5 @@ if (theScore == 3) {
 }
 if (theScore >=0 || theScore <=2) {
     theTxt.textContent = "You are below average intelligence!";
+}
 }
