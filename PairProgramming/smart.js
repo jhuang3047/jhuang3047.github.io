fab84d980026b09aa3faf53751963215e78a8f17
jhuang3047 <60354062+jhuang3047@users.noreply.github.com>
2020-04-26 00:24:34 -0400

    updated

--- PairProgramming/smart.js
+++ PairProgramming/smart.js
@@ -2,7 +2,7 @@
 
 var theScore = 0 ;
 //var wrongButton = $(".wrongButton",".squarewrongButton1",".squarewrongButton2",".squarewrongButton3",".circlewrongButton1",".circlewrongButton2",".circlewrongButton3",".rectanglewrongButton1",".rectanglewrongButton2",".rectanglewrongButton3");
-var correctButton =$(".correctButton",".squarecorrectButton",".rectanglecorrectButton",".circlecorrectButton");
+var correctButton =$("#correctButton","#squarecorrectButton","#rectanglecorrectButton","#circlecorrectButton");
 var head2 = $("#head2");
 
 correctButton.click(function(){
