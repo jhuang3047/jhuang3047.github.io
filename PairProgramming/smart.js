cae7518ce6c811750183afbc0c9b50007f07aa47
jhuang3047 <60354062+jhuang3047@users.noreply.github.com>
2020-04-26 00:18:34 -0400

    updated

--- PairProgramming/smart.js
+++ PairProgramming/smart.js
@@ -1,25 +1,26 @@
 
-    
+
 var theScore = 0 ;
-var wrongButton = document.getElementById("wrongButton","squarewrongButton1","squarewrongButton2","squarewrongButton3","circlewrongButton1","circlewrongButton2","circlewrongButton3","rectanglewrongButton1","rectanglewrongButton2","rectanglewrongButton3");
-var correctButton =document.getElementById("correctButton","squarecorrectButton","rectanglecorrectButton","circlecorrectButton")
-var theText = document.querySelector('h2')
+//var wrongButton = $(".wrongButton",".squarewrongButton1",".squarewrongButton2",".squarewrongButton3",".circlewrongButton1",".circlewrongButton2",".circlewrongButton3",".rectanglewrongButton1",".rectanglewrongButton2",".rectanglewrongButton3");
+var correctButton =$(".correctButton",".squarecorrectButton",".rectanglecorrectButton",".circlecorrectButton");
+var head2 = $("#head2");
 
-correctButton.onclick = function(){
+correctButton.click(function(){
     theScore = +1
-}
-wrongButton.onclick = function(){
-    theScore =+0
-}
+})
+// wrongButton.click(function(){
+//     theScore =+0
+// })
 
+function average(){
 if (theScore <= 4) {
-    theTxt.textContent = "You are above average intelligence!";
+    head2.text = ('You are above average intelligence!')
 
 }
-if (theScore == 3) {
-    theTxt.textContent = "You are of average intelligence!";
+if (theScore ==3) {
+    head2.text = ('You are of average intelligence!')
 }
 if (theScore >=0 || theScore <=2) {
-    theTxt.textContent = "You are below average intelligence!";
+    head2.text = ('You are below average intelligence!')
 
-}
+}}
