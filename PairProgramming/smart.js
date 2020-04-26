e768668938dae424b21316ba5f0d5ea55ed3f02e
jhuang3047 <60354062+jhuang3047@users.noreply.github.com>
2020-04-26 01:05:53 -0400

    updated

--- PairProgramming/smart.js
+++ PairProgramming/smart.js
@@ -2,49 +2,30 @@
 
 var theScore = x ;
 var x = 5;
-var wrongButton1 = $("#wrongButton")
-var wrongButton2 =$("#squarewrongButton1");
-var wrongButton3 =$("#squarewrongButton2");
-var wrongButton4= $("#squarewrongButton3");
-var wrongButton5=$("#circlewrongButton1");
-var wrongButton6=$("#circlewrongButton2");
-var wrongButton7=$("#circlewrongButton3");
-var wrongButton8=$("#rectanglewrongButton1")
-var wrongButton9=$("#rectanglewrongButton2")
-var wrongButton10=$("#rectanglewrongButton3");
+var wrongButton1 = $("#wrongButton").click(function(){
+    theScore = x - 1
+})
+var wrongButton2 =$("#squarewrongButton1").click(function(){
+    theScore = x - 1
+var wrongButton3 =$("#squarewrongButton2").click(function(){
+    theScore = x - 1
+var wrongButton4= $("#squarewrongButton3").click(function(){
+    theScore = x - 1
+var wrongButton5=$("#circlewrongButton1").click(function(){
+    theScore = x - 1
+var wrongButton6=$("#circlewrongButton2").click(function(){
+    theScore = x - 1
+var wrongButton7=$("#circlewrongButton3").click(function(){
+    theScore = x - 1
+var wrongButton8=$("#rectanglewrongButton1").click(function(){
+    theScore = x - 1
+var wrongButton9=$("#rectanglewrongButton2").click(function(){
+    theScore = x - 1
+var wrongButton10=$("#rectanglewrongButton3").click(function(){
+    theScore = x - 1
 //var correctButton =$("#correctButton","#squarecorrectButton","#rectanglecorrectButton","#circlecorrectButton");
 var head2 = $("#head2");
 
-wrongButton1.click(function(){
-    theScore = x - 1
-})
-wrongButton2.click(function(){
-    theScore = x - 1
-})
-wrongButton3.click(function(){
-    theScore = x - 1
-})
-wrongButton4.click(function(){
-    theScore = x - 1
-})
-wrongButton5.click(function(){
-    theScore = x - 1
-})
-wrongButton6.click(function(){
-    theScore = x - 1
-})
-wrongButton7.click(function(){
-    theScore = x - 1
-})
-wrongButton8.click(function(){
-    theScore = x - 1
-})
-wrongButton9.click(function(){
-    theScore = x - 1
-})
-wrongButton10.click(function(){
-    theScore = x - 1
-})
 
 
 function above(){
