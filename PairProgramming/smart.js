2256210fd15b4b36af822a54d189e7da09ae9c6d
jhuang3047 <60354062+jhuang3047@users.noreply.github.com>
2020-04-26 01:13:06 -0400

    updated

--- PairProgramming/smart.js
+++ PairProgramming/smart.js
@@ -2,26 +2,26 @@
 
 var theScore = x ;
 var x = 5;
-var wrongButton1 = $("#wrongButton").click(function(){
+var wrongButton1 = $("#wrongButton").onclick(function(){
     theScore = x - 1
 })
-var wrongButton2 =$("#squarewrongButton1").click(function(){
+var wrongButton2 =$("#squarewrongButton1").onclick(function(){
     theScore = x - 1
-var wrongButton3 =$("#squarewrongButton2").click(function(){
+var wrongButton3 =$("#squarewrongButton2").onclick(function(){
     theScore = x - 1
-var wrongButton4= $("#squarewrongButton3").click(function(){
+var wrongButton4= $("#squarewrongButton3").onclick(function(){
     theScore = x - 1
-var wrongButton5=$("#circlewrongButton1").click(function(){
+var wrongButton5=$("#circlewrongButton1").onclick(function(){
     theScore = x - 1
-var wrongButton6=$("#circlewrongButton2").click(function(){
+var wrongButton6=$("#circlewrongButton2").onclick(function(){
     theScore = x - 1
-var wrongButton7=$("#circlewrongButton3").click(function(){
+var wrongButton7=$("#circlewrongButton3").onclick(function(){
     theScore = x - 1
-var wrongButton8=$("#rectanglewrongButton1").click(function(){
+var wrongButton8=$("#rectanglewrongButton1").onclick(function(){
     theScore = x - 1
-var wrongButton9=$("#rectanglewrongButton2").click(function(){
+var wrongButton9=$("#rectanglewrongButton2").onclick(function(){
     theScore = x - 1
-var wrongButton10=$("#rectanglewrongButton3").click(function(){
+var wrongButton10=$("#rectanglewrongButton3").onclick(function(){
     theScore = x - 1
 //var correctButton =$("#correctButton","#squarecorrectButton","#rectanglecorrectButton","#circlecorrectButton");
 var head2 = $("#head2");
@@ -30,15 +30,15 @@ var head2 = $("#head2");
 
 function above(){
 if (theScore >= 4) {
-    head2.text = ('You are a genius!')
+    head2.text('You are a genius!')
 
 }}
 function average(){
 if (theScore === 3) {
-    head2.text = ('You are of average intelligence!')
+    head2.text('You are of average intelligence!')
 }}
 function below(){
 if (theScore  <= 2) {
-    head2.text = ('You are below average intelligence!')
+    head2.text('You are below average intelligence!')
 
 }}
