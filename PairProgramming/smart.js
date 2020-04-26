139fc1ea0e0e1b592fec53d555ba403a956e46b7
jhuang3047 <60354062+jhuang3047@users.noreply.github.com>
2020-04-26 19:49:56 -0400

    updated

--- PairProgramming/smart.js
+++ PairProgramming/smart.js
@@ -11,36 +11,36 @@ wrongButton2.onclick = function(){
 }
 var wrongButton3 =$("#squarewrongButton2")
 wrongButton3.onclick = function(){
-    theScore = x - 1
+    theScore =  theScore - 1
 }
 var wrongButton4= $("#squarewrongButton3")
 wrongButton4.onclick = function(){
-    theScore = x - 1
+    theScore =  theScore - 1
 }
 var wrongButton5=$("#circlewrongButton1")
 wrongButton5.onclick = function(){
-    theScore = x - 1
+    theScore =  theScore - 1
 }
 var wrongButton6=$("#circlewrongButton2")
 wrongButton6.onclick = function(){
-    theScore = x - 1
+    theScore =  theScore - 1
 }
 var wrongButton7=$("#circlewrongButton3");
 wrongButton7.onclick = function(){
-    theScore = x - 1
+    theScore =  theScore - 1
 }
 var wrongButton8=$("#rectanglewrongButton1");
 wrongButton8.onclick = function(){
-    theScore = x - 1
+    theScore =  theScore - 1
 
 }
 var wrongButton9=$("#rectanglewrongButton2");
 wrongButton9.onclick = function(){
-    theScore = x - 1
+    theScore =  theScore - 1
 }
 var wrongButton10=$("#rectanglewrongButton3");
 wrongButton10.onclick = function(){
-    theScore = x - 1
+    theScore =  theScore - 1
 }
 //var correctButton =$("#correctButton","#squarecorrectButton","#rectanglecorrectButton","#circlecorrectButton");
 var head2 = $("#head2");
@@ -50,8 +50,8 @@ var head2 = $("#head2");
 //     theScore = x ;
 // })
 var gameStart= $("endButton").click(function(){
-    x = 5 ;
-    theScore = x ;
+   
+    theScore = 5 ;
 })
 
 function above(){
