82871eeaf385bd8c531e7830ec2f1fa1f4ae7d1d
jhuang3047 <60354062+jhuang3047@users.noreply.github.com>
2020-04-26 00:58:24 -0400

    updated

--- PairProgramming/smart.js
+++ PairProgramming/smart.js
@@ -1,20 +1,55 @@
 
 
-var theScore = 0 ;
-//var wrongButton = $(".wrongButton",".squarewrongButton1",".squarewrongButton2",".squarewrongButton3",".circlewrongButton1",".circlewrongButton2",".circlewrongButton3",".rectanglewrongButton1",".rectanglewrongButton2",".rectanglewrongButton3");
-var correctButton =$("#correctButton","#squarecorrectButton","#rectanglecorrectButton","#circlecorrectButton");
+var theScore = x ;
+var x = 5;
+var wrongButton1 = $("#wrongButton")
+var wrongButton2 =$("#squarewrongButton1");
+var wrongButton3 =$("#squarewrongButton2");
+var wrongButton4= $("#squarewrongButton3");
+var wrongButton5=$("#circlewrongButton1");
+var wrongButton6=$("#circlewrongButton2");
+var wrongButton7=$("#circlewrongButton3");
+var wrongButton8=$("#rectanglewrongButton1")
+var wrongButton9=$("#rectanglewrongButton2")
+var wrongButton10=$("#rectanglewrongButton3");
+//var correctButton =$("#correctButton","#squarecorrectButton","#rectanglecorrectButton","#circlecorrectButton");
 var head2 = $("#head2");
 
-correctButton.click(function(){
-    theScore + 1
+wrongButton1.click(function(){
+    theScore = x - 1
 })
-// wrongButton.click(function(){
-//     theScore =+0
-// })
+wrongButton2.click(function(){
+    theScore = x - 1
+})
+wrongButton3.click(function(){
+    theScore = x - 1
+})
+wrongButton4.click(function(){
+    theScore = x - 1
+})
+wrongButton5.click(function(){
+    theScore = x - 1
+})
+wrongButton6.click(function(){
+    theScore = x - 1
+})
+wrongButton7.click(function(){
+    theScore = x - 1
+})
+wrongButton8.click(function(){
+    theScore = x - 1
+})
+wrongButton9.click(function(){
+    theScore = x - 1
+})
+wrongButton10.click(function(){
+    theScore = x - 1
+})
+
 
 function above(){
 if (theScore >= 4) {
-    head2.text = ('You are above average intelligence!')
+    head2.text = ('You are a genius!')
 
 }}
 function average(){
