4dbf1d3cff329a8a609ece437b8f4d7f1a6b8fff
jhuang3047 <60354062+jhuang3047@users.noreply.github.com>
2020-04-26 23:43:43 -0400

    updated

--- smart/bleh.js
+++ smart/bleh.js
@@ -1,4 +1,9 @@
 var head2 = document.querySelector('h2');
+var theButton1 = document.querySelector("wrongButton1");
+var theButton2 = document.querySelector("wrongButton2");
+var theButton3 = document.querySelector("wrongButton3");
+
+
     var count =(function(){
     var counter =0;
         return function(){return counter +=1;}
