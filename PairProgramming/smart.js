49a309caca850250b17f2ca0e996ec67a02167c3
jhuang3047 <60354062+jhuang3047@users.noreply.github.com>
2020-04-26 23:02:47 -0400

    updated

--- PairProgramming/smart.js
+++ PairProgramming/smart.js
@@ -1,75 +1,23 @@
-var theScore ;
+var head2 = document.querySelector('h2');
+    var count =(function(){
+    var counter =0;
+        return function(){return counter +=1;}
 
-
-var wrongButton1 = $("#wrongButton");
-wrongButton1.onclick = function(){
-    theScore = theScore - 1;
-    
-}
-var wrongButton2 =$("#squarewrongButton1");
-wrongButton2.onclick = function(){
-
-    theScore = theScore - 1;
-}
-var wrongButton3 =$("#squarewrongButton2");
-wrongButton3.onclick = function(){
-    
-    theScore = theScore - 1;
-}
-var wrongButton4= $("#squarewrongButton3");
-wrongButton4.onclick = function(){
-    
-    theScore = theScore - 1;
-}
-var wrongButton5=$("#circlewrongButton1");
-wrongButton5.onclick = function(){
-    
-    theScore = theScore - 1;
-}
-var wrongButton6=$("#circlewrongButton2");
-wrongButton6.onclick = function(){
-    
-    theScore = theScore - 1;
-}
-var wrongButton7=$("#circlewrongButton3");
-wrongButton7.onclick = function(){
-    theScore = theScore - 1;
-}
-var wrongButton8=$("#rectanglewrongButton1");
-wrongButton8.onclick = function(){
-    theScore = theScore - 1;
-}
-var wrongButton9=$("#rectanglewrongButton2");
-wrongButton9.onclick = function(){
-    theScore = theScore - 1;
-}
-var wrongButton10=$("#rectanglewrongButton3");
-wrongButton10.onclick = function(){
-    theScore = theScore - 1;
-}
-//var correctButton =$("#correctButton","#squarecorrectButton","#rectanglecorrectButton","#circlecorrectButton");
-var head2 = $("#head2");
-
-// var gameEnd= $("#startButton").click(function(){
-//     x = 5 ;
-//     theScore = x ;
-// })
-var gameStart= $("startButton");
-gameStart.onclick = function(){
-   
-    theScore = 5;
-}
-
+    })()
+    function displaycount(){
+        document.getElementById("carrier").innerHTML =count();
+        }
 function above(){
-if (theScore > 3) {
-    head2.text('You are a genius!');
-
-}
-else if (theScore == 3) {
-    head2.text('You are of average intelligence!');
-}
-
-else if (theScore < 3) {
-    head2.text('You are below average intelligence!');
-
-}}
+    if (counter > 3) {
+        head2.text('You are a genius!');
+    
+    }
+    else if (counter == 3) {
+        head2.text('You are of average intelligence!');
+    }
+    
+    else if (counter < 3) {
+        head2.text('You are below average intelligence!');
+    
+    }}
+    
